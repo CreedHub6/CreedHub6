f3f4eca0a6d45685a08383fabbb537a00e3d0900
CreedHub6 <liljezzro@gmail.com>
2025-08-14 13:09:05 +0100

    Integrate Cloudinary storage with .env.local and add TestImage model for verification

--- onlinefoodorderingsystem/settings.py
+++ onlinefoodorderingsystem/settings.py
@@ -109,12 +109,7 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
 
-# ---------------------------------------------------------------------
-# Static files (CSS, JS)
-# ---------------------------------------------------------------------
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 # ---------------------------------------------------------------------
 # Media storage (Cloudinary)
@@ -124,7 +119,14 @@ CLOUDINARY_STORAGE = {
     'CLOUD_NAME': env('CLOUDINARY_CLOUD_NAME'),
     'API_KEY': env('CLOUDINARY_API_KEY'),
     'API_SECRET': env('CLOUDINARY_API_SECRET'),
-}
+
+# ---------------------------------------------------------------------
+# Static files (CSS, JS)
+# ---------------------------------------------------------------------
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 # ---------------------------------------------------------------------
 # Default primary key field type
