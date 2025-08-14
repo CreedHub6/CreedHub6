97c66c7eb3399bfc80380788d7fdce98a5597f4e
CreedHub6 <liljezzro@gmail.com>
2025-08-14 14:00:08 +0100

    Integrate Cloudinary storage with .env.local and add TestImage model for verification

--- systemapp/models.py
+++ systemapp/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
+from cloudinary.models import CloudinaryField
+
 class Restaurant(models.Model):
     name = models.CharField(max_length=100)
     address = models.TextField()
@@ -15,7 +18,7 @@ class MenuItem(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    image = models.ImageField(upload_to='menu_items/', blank=True, null=True)
+    image = CloudinaryField('menu_items', blank=True, null=True)
 
     def __str__(self):
         return self.name
@@ -66,7 +69,6 @@ class CartItem(models.Model):
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
     menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=1)
-
     def __str__(self):
         return f"{self.menu_item.name} in {self.cart.user.username}'s cart"
 
